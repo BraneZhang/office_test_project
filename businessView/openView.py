4343c96d18c7e06c78995e6890e12a443fe97201
Art3YouLi <excel1entxx@126.com>
2019-10-10 10:37:42 +0800

    新增方法： 1.验证文件是否被关闭 2.打开搜索结果列表随机文件

--- businessView/openView.py
+++ businessView/openView.py
@@ -3,6 +3,7 @@
 import logging
 import os
 import time
+import random
 
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
@@ -16,6 +17,22 @@ class OpenView(Common):
     def close_file(self):
         logging.info('======close_file=====')
         self.driver.find_element(By.ID, 'com.yozo.office:id/yozo_ui_toolbar_button_close').click()  # 关闭功能
+
+    def check_close_file(self):
+        """
+        验证文档是否被关闭
+        :return: flag
+        """
+        logging.info('==========check_close_file==========')
+        time.sleep(1)
+        flag = False
+        try:
+            self.driver.find_element(By.ID, 'com.yozo.office:id/yozo_ui_toolbar_button_close')
+        except NoSuchElementException:
+            logging.info('==========the file closed==========')
+            flag = True
+
+        return flag
 
     def open_file(self, file_name):
         logging.info('======open_file_%s=====' % file_name)
@@ -38,3 +55,15 @@ class OpenView(Common):
         else:
             logging.info(file_name + 'open success!')
             return True
+
+    def open_random_file(self, keywords):
+        logging.info('======open_random_file=====')
+        self.driver.find_element(By.ID, 'com.yozo.office:id/im_title_bar_menu_search').click()  # 点击搜索功能
+        self.driver.find_element(By.ID, 'com.yozo.office:id/et_search').send_keys(keywords)  # 输入关键字
+        self.driver.find_element(By.ID, 'com.yozo.office:id/iv_search_search').click()  # 点击搜索按钮
+        time.sleep(1)
+        results_list = self.driver.find_elements(By.ID, 'com.yozo.office:id/file_item')
+        num = random.randint(0, len(results_list)-1)
+        result = self.driver.find_elements(By.ID, 'com.yozo.office:id/file_item')[num]
+        result.click()
+
