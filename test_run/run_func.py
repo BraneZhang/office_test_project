08a51e646271a806dacfc25869f1add58c0b2dc5
branezhang <branezhang@163.com>
2019-11-06 17:36:31 +0800

    添加此文件作为日常功能用例的起始点，不受其他需求脚本干扰

---
+++ test_run/run_func.py
@@ -0,0 +1,23 @@
+import HTMLTestRunner
+import os
+import unittest
+import time, logging
+import sys
+
+
+path = os.path.dirname(os.getcwd())
+sys.path.append(path)
+
+test_dir = '../test_case2'
+report_dir = '../reports'
+# test_dir = 'D:/PycharmProjects/office_test_project/test_case'
+# report_dir = 'D:/PycharmProjects/office_test_project/reports'
+
+discover = unittest.defaultTestLoader.discover(test_dir, pattern='test*.py')
+now = time.strftime('%Y-%m-%d %H_%M_%S')
+report_name = report_dir + '/' + now + 'Mobile_Office_Report.html'
+with open(r'%s' % report_name, 'wb') as f:
+    runner = HTMLTestRunner.HTMLTestRunner(stream=f, title='YOZO_Mobile_Office_Report',
+                                           description='yozo Android app test report',retry=3)
+    logging.info('start run test case...')
+    runner.run(discover)
