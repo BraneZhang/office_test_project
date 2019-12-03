c73a88cbc307102a3f8f2ef7fd764295389e09b9
USER1057 <dzh@yozosoft.com>
2019-12-03 15:01:40 +0800

    增加在线、离线打开按钮

--- businessView/openView.py
+++ businessView/openView.py
@@ -39,8 +39,11 @@ class OpenView(Common):
         self.driver.find_element(By.ID, 'com.yozo.office:id/im_title_bar_menu_search').click()  # 点击搜索功能
         self.driver.find_element(By.ID, 'com.yozo.office:id/et_search').send_keys(file_name)  # 输入搜索内容
         self.driver.find_element(By.ID, 'com.yozo.office:id/iv_search_search').click()  # 点击搜索按钮
+        while not self.exist('//android.widget.TextView[@text="%s"]' % file_name):
+            time.sleep(2)
         self.driver.find_element(By.XPATH, '//android.widget.TextView[@text="%s"]' % file_name).click()  # 打开对应文件
-        time.sleep(5)
+        while not self.exist('//*[@resource-id="com.yozo.office:id/yozo_ui_option_group_button"]'):
+            time.sleep(2)
 
     def check_open_status(self, file_name):
         logging.info('======test_open_status_%s=====' % file_name)
