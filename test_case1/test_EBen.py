a6e76d4364b1b519fab4ae27f1b93f920a7217a2
branezhang <branezhang@163.com>
2020-01-17 14:00:32 +0800

    编辑保存更新

--- test_case1/test_EBen.py
+++ test_case1/test_EBen.py
@@ -19,53 +19,65 @@ class TestEBen(StartEnd):
         logging.info('==========test_new_file_edit_30min_save==========')
         hp = HomePageView(self.driver)
         time.sleep(2)
-        hp.tap(1430, 1780)
+        # hp.tap(1430, 1780)
+        # hp.tap(925, 1559)
+        hp.tap(925, 1669)
         time.sleep(2)
         self.driver.find_element(By.XPATH, '//android.widget.ImageButton[3]').click()
         time.sleep(1)
-        null_file = '//*[@resource-id="com.yozo.office:id/create_empty_offline_img"]'
+        null_file = '//*[@resource-id="com.yozo.office:id/createEmpty"]'
+        if not hp.exist(null_file):
+            null_file = '//*[@resource-id="com.yozo.office:id/create_empty_offline_img"]'
         self.driver.find_element(By.XPATH, null_file).click()
 
-        #编辑
-        self.driver.find_element(By.ID, 'com.yozo.office:id/a0000_scale_motion_helper_layout_id').click()  # 进入编辑状态
-        start_time = time.time()
-        # for i in range(100):
-        #     logging.info('>>>>>>>>No.%s' % i)
-        #     self.driver.keyevent(randint(29, 54))
-        end_time = time.time()
-        last_time = end_time - start_time
-        print(f'las_ttime:{last_time}')
-
-        while last_time < 32 * 60:
-            for i in range(100):
-                logging.info('>>>>>>>>No.%s'%i)
-                self.driver.keyevent(randint(29, 54))
-            end_time = time.time()
-            last_time = end_time - start_time
-            logging.info('last_time>>>>>>>>%s' % last_time)
-
-        hp.save_new_file('new_edit30save.doc','local')
+        hp.save_new_file('new_edit30save.doc', 'local')
         self.assertTrue(hp.get_toast_message('保存成功'))
 
+        # 编辑
         self.driver.find_element(By.ID, 'com.yozo.office:id/a0000_scale_motion_helper_layout_id').click()  # 进入编辑状态
         start_time = time.time()
-        # for i in range(100):
-        #     logging.info('>>>>>>>>No.%s' % i)
-        #     self.driver.keyevent(randint(29, 54))
+        for i in range(100):
+            logging.info('>>>>>>>>No.%s' % i)
+            self.driver.keyevent(randint(29, 54))
         end_time = time.time()
         last_time = end_time - start_time
-        print(f'las_ttime:{last_time}')
+        # print(f'las_ttime:{last_time}')
+        logging.info('last_time>>>>>>>>%s' % last_time)
 
-        while last_time < 32 * 60:
+        while last_time < 2 * 60 * 60:
             for i in range(100):
                 logging.info('>>>>>>>>No.%s' % i)
                 self.driver.keyevent(randint(29, 54))
             end_time = time.time()
             last_time = end_time - start_time
             logging.info('last_time>>>>>>>>%s' % last_time)
+            hp.save_file()
+            self.assertTrue(hp.get_toast_message('保存成功'))
 
-        hp.save_file()
-        self.assertTrue(hp.get_toast_message('保存成功'))
+        # hp.save_file()
+        # self.assertTrue(hp.get_toast_message('保存成功'))
+        # hp.save_new_file('new_edit30save.doc','local')
+        # self.assertTrue(hp.get_toast_message('保存成功'))
+
+        # self.driver.find_element(By.ID, 'com.yozo.office:id/a0000_scale_motion_helper_layout_id').click()  # 进入编辑状态
+        # start_time = time.time()
+        # # for i in range(100):
+        # #     logging.info('>>>>>>>>No.%s' % i)
+        # #     self.driver.keyevent(randint(29, 54))
+        # end_time = time.time()
+        # last_time = end_time - start_time
+        # print(f'las_ttime:{last_time}')
+        #
+        # while last_time < 32 * 60:
+        #     for i in range(100):
+        #         logging.info('>>>>>>>>No.%s' % i)
+        #         self.driver.keyevent(randint(29, 54))
+        #     end_time = time.time()
+        #     last_time = end_time - start_time
+        #     logging.info('last_time>>>>>>>>%s' % last_time)
+        #
+        # hp.save_file()
+        # self.assertTrue(hp.get_toast_message('保存成功'))
 
     @unittest.skip("贵州政务")
     def test_new_file_edit_30min_save(self):
