13518165a0bc4284e81eb61d3074e6b1292d8113
branezhang <branezhang@163.com>
2020-02-27 15:07:57 +0800

    添加批量开档的启动文件

---
+++ test_run/run_batch_open.py
@@ -0,0 +1,24 @@
+import HTMLTestRunner
+import os
+import unittest
+import time, logging
+import sys
+
+from common.tool import Device_Select
+
+path = os.path.dirname(os.getcwd())
+sys.path.append(path)
+
+test_dir = '../test_case1'
+report_dir = '../reports'
+# test_dir = 'D:/PycharmProjects/office_test_project/test_case'
+# report_dir = 'D:/PycharmProjects/office_test_project/reports'
+
+discover = unittest.defaultTestLoader.discover(test_dir, pattern='test_open_files.py')
+now = time.strftime('%Y-%m-%d %H_%M_%S')
+report_name = report_dir + '/' + now + 'Mobile_Office_Report.html'
+with open(r'%s' % report_name, 'wb') as f:
+    runner = HTMLTestRunner.HTMLTestRunner(stream=f, title='YOZO_Mobile_Office_Report',
+                                           description='yozo Android app test report')
+    logging.info('start run test case...')
+    runner.run(discover)
