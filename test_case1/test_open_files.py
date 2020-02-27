6da819725aee64226ba108e34571ecb1c3c306bf
branezhang <branezhang@163.com>
2020-02-27 16:57:25 +0800

    修改开档判定操作

--- test_case1/test_open_files.py
+++ test_case1/test_open_files.py
@@ -7,7 +7,7 @@ from ddt import ddt, data
 from businessView.homePageView import HomePageView
 from common.myunit import StartEnd
 
-path1 = r'E:\MSfiles\MS2003files\xls\23000-23999'
+path1 = r'E:\MSfiles\MS2003files\xls\20000-20999'
 file_names = []
 for root, dirs, files in os.walk(path1):
     for f in files:
@@ -17,23 +17,24 @@ for root, dirs, files in os.walk(path1):
 @ddt
 class OpenFiles(StartEnd):
 
-    @data(*file_names)
+    @data(*file_names[45:46])
     def test_bat_open_files(self, file_name=''):
         logging.info('==========正在打开%s==========' % file_name)
-        try:
-            type1 = os.path.splitext(file_name)[1]
-            if type1 != '.xls' and type1 != '.xlsx':
-                raise Exception("文件格式为%s" % type1)
-            hp = HomePageView(self.driver)
-            hp.search_file(file_name)
-            hp.open_file(file_name)
-            hp.close_file()
-            self.assertTrue(hp.check_close_file(), msg='文档关闭失败')
-        except Exception as e:
-            logging.info(e)
-            PC_path = os.path.join(os.path.expanduser("~"), 'Desktop', 'wrong')
-            shutil.copy(os.path.join(path1, file_name), PC_path)
-            raise
+
+        type1 = os.path.splitext(file_name)[1]
+        pc_path = os.path.join(os.path.expanduser("~"), 'Desktop', 'wrong')
+        if type1 != '.xls' and type1 != '.xlsx':
+            shutil.copy(os.path.join(path1, file_name), pc_path)
+            self.assertTrue(False, 'file format wrong')
+        hp = HomePageView(self.driver)
+        search_result = hp.search_file(file_name)
+        self.assertTrue(search_result, 'cannot find file')
+        open_result = hp.open_file(file_name)
+        if not open_result:
+            shutil.copy(os.path.join(path1, file_name), pc_path)
+        self.assertTrue(open_result, 'open file failed')
+        hp.close_file()
+        self.assertTrue(hp.check_close_file(), msg='close file failed')
 
 
 if __name__ == '__main__':
